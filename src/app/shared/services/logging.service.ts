4ed1115174ce19a2529beefbd2004a27de368107
Enis Belli <Enis.Belli@LMIS.de>
2022-07-05 10:26:22 +0200

    PR 18289 (Merge feature/54119-dependency-updates to develop) gemergt
    Verknüpfte Arbeitselemente: #54119

--- src/app/shared/services/logging.service.ts
+++ src/app/shared/services/logging.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 /**
  * Base class for a logging service. An implementation can be provided based on the environment.
@@ -12,10 +12,15 @@ export abstract class LoggingService {
    * Sets the user id of the authenticated user so it can be used in log messages.
    * @param userId User Id.
    */
-  abstract setUserId(userId: string): void;
-  abstract logException(exception: Error): void;
-  abstract logDebug(message: string): void;
-  abstract logInfo(message: string): void;
-  abstract logWarn(message: string): void;
-  abstract logError(message: string): void;
+  public abstract setUserId(userId: string): void;
+
+  public abstract logException(exception: Error): void;
+
+  public abstract logDebug(message: string): void;
+
+  public abstract logInfo(message: string): void;
+
+  public abstract logWarn(message: string): void;
+
+  public abstract logError(message: string): void;
 }
