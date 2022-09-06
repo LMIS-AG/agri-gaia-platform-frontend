289076e933731e50a82b3aa908edbe7073522dcf
mgr <mgr@lmis.de>
2022-09-06 09:57:15 +0200

    resolved merge conflict

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,11 +8,7 @@ export const environment = {
     url: 'https://account.platform.agri-gaia.com/auth',
     realm: 'agri-gaia',
     clientId: 'platform-ui',
-<<<<<<< HEAD
   },
-=======
-  }
->>>>>>> 6305f303ffc90a9a2fcb54e8965b922900124a5b
 };
 
 /*
