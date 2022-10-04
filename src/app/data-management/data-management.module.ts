be829900eb426080048ff3becc43927c99273143
mgr <mgr@lmis.de>
2022-10-04 13:09:58 +0200

    moved folder

--- src/app/data-management/data-management.module.ts
+++ src/app/data-management/data-management.module.ts
@@ -17,7 +17,7 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { PoliciesComponent } from './pages/policies/policies.component';
 import { CreatePolicyComponent } from './pages/policies/create-policy/create-policy.component';
 import { MatSelectModule } from '@angular/material/select';
-import { ConstraintsComponent } from './pages/policies/createPolicy/constraints/constraints.component';
+import { ConstraintsComponent } from './pages/policies/create-policy/constraints/constraints.component';
 
 const routes: Routes = [
   {
