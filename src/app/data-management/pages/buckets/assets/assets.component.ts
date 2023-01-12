772797adfb2e94fa3a5ef83d832c5c313dd6bca0
Julian-Marco <93761466+Julian-Marco@users.noreply.github.com>
2023-01-12 09:47:16 +0100

    Feature/56963 unpublish asset (#14)
    * Added button for unpublishing assets
    
    * Changed confirm text for deletion
    
    Co-authored-by: ebe-lmis <Enis.Belli@LMIS.de>

--- src/app/data-management/pages/buckets/assets/assets.component.ts
+++ src/app/data-management/pages/buckets/assets/assets.component.ts
@@ -15,7 +15,7 @@ import { translate } from '@ngneat/transloco';
 export class AssetsComponent implements OnInit {
   public bucket?: string;
 
-  public displayedColumnsDataset: string[] = ['name', 'date', 'publish-button'];
+  public displayedColumnsDataset: string[] = ['name', 'date', 'buttons'];
   public datasetDatasource: GeneralPurposeAsset[] = [];
 
   constructor(
