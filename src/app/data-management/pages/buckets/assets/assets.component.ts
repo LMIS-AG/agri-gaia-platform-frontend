41da2fb7bae4e663274a31289212ab85ceee1fae
Marcel Ruland <marcel.ruland@lmis.de>
2023-01-13 10:24:36 +0100

    Confirmation toast for successful/failed publish/unpublish actions

--- src/app/data-management/pages/buckets/assets/assets.component.ts
+++ src/app/data-management/pages/buckets/assets/assets.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute } from '@angular/router';
-import { filter, map, switchMap } from 'rxjs';
-import { BucketService } from '../bucket.service';
-import { GeneralPurposeAsset } from '../../../../shared/model/coopSpaceAsset';
-import { UIService } from '../../../../shared/services/ui.service';
-import { translate } from '@ngneat/transloco';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {ActivatedRoute} from '@angular/router';
+import {catchError, filter, map, switchMap} from 'rxjs';
+import {BucketService} from '../bucket.service';
+import {GeneralPurposeAsset} from '../../../../shared/model/coopSpaceAsset';
+import {UIService} from '../../../../shared/services/ui.service';
+import {translate} from '@ngneat/transloco';
+import {HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-assets',
@@ -22,8 +23,9 @@ export class AssetsComponent implements OnInit {
     private route: ActivatedRoute,
     private bucketService: BucketService,
     private dialog: MatDialog,
-    private ui: UIService
-  ) {}
+    private uiService: UIService
+  ) {
+  }
 
   public ngOnInit(): void {
     this.route.paramMap
@@ -31,7 +33,7 @@ export class AssetsComponent implements OnInit {
         filter(paramMap => paramMap.has('name')),
         map(paramMap => paramMap.get('name')),
         switchMap(name =>
-          this.bucketService.getAssetsByBucketName(name ? name : '').pipe(map(assets => ({ name, assets })))
+          this.bucketService.getAssetsByBucketName(name ? name : '').pipe(map(assets => ({name, assets})))
         )
       )
       .subscribe(result => {
@@ -41,30 +43,56 @@ export class AssetsComponent implements OnInit {
   }
 
   public publishAsset(element: GeneralPurposeAsset): void {
-    this.ui
-      .confirm(`${element.name}`, translate('dataManagement.buckets.assets.dialog.confirmationQuestion'), {
-        confirmationText: translate('dataManagement.buckets.assets.dialog.confirmationText'),
+    this.uiService
+      .confirm(`${element.name}`, translate('dataManagement.buckets.assets.dialog.publishConfirmationQuestion'), {
+        // TODO: This argument isn't used anywhere.
+        confirmationText: translate('dataManagement.buckets.assets.dialog.publishConfirmationText'),
         buttonLabels: 'confirm',
         confirmButtonColor: 'primary',
       })
-      .subscribe(result => {
-        if (result) {
-          this.bucketService.publish(this.bucket!, element.name);
-        }
-      });
+      .subscribe((userConfirmed: boolean) => {
+        if (!userConfirmed) return;
+        let bucket = this.bucket;
+        if (bucket == null) throw Error("Bucket was null in publishAsset().");
+        this.bucketService.publishAsset(bucket, element.name).subscribe({
+          next: response => this.handlePublishSuccess(response),
+          error: err => this.handlePublishError(err),
+        });
+      })
   }
 
   public unpublishAsset(element: GeneralPurposeAsset): void {
-    this.ui
-      .confirm(`${element.name}`, translate('dataManagement.buckets.assets.dialog.deleteConfirmationQuestion'), {
-        confirmationText: translate('dataManagement.buckets.assets.dialog.deleteConfirmationText'),
+    this.uiService
+      .confirm(`${element.name}`, translate('dataManagement.buckets.assets.dialog.unpublishConfirmationQuestion'), {
+        // TODO: This argument isn't used anywhere.
+        confirmationText: translate('dataManagement.buckets.assets.dialog.unpublishConfirmationText'),
         buttonLabels: 'confirm',
-        confirmButtonColor: 'primary',
+        confirmButtonColor: 'warn',
       })
-      .subscribe(result => {
-        if (result) {
-          this.bucketService.unpublishAsset(this.bucket!, element.name);
-        }
+      .subscribe((userConfirmed: boolean) => {
+        if (!userConfirmed) return;
+        let bucket = this.bucket;
+        if (bucket == null) throw Error("Bucket was null in unpublishAsset().");
+        this.bucketService.unpublishAsset(bucket, element.name).subscribe({
+          next: response => this.handleUnpublishSuccess(response),
+          error: err => this.handleUnpublishError(err),
+        });
       });
+  }
+
+  public handlePublishSuccess(response: HttpResponse<unknown>): void {
+    this.uiService.showSuccessMessage(translate('dataManagement.buckets.assets.dialog.publishConfirmationText'))
+  }
+
+  public handlePublishError(err: any): void {
+    this.uiService.showErrorMessage(translate('dataManagement.buckets.assets.dialog.publishErrorText') + err.status)
+  }
+
+  public handleUnpublishSuccess(response: HttpResponse<unknown>): void {
+    this.uiService.showSuccessMessage(translate('dataManagement.buckets.assets.dialog.unpublishConfirmationText'))
+  }
+
+  public handleUnpublishError(err: any): void {
+    this.uiService.showErrorMessage(translate('dataManagement.buckets.assets.dialog.unpublishErrorText') + err.status)
   }
 }
