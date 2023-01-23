c57b504135303fd932d76d61bcf89a7dafe710aa
Marcel Ruland <marcel.ruland@lmis.de>
2023-01-23 12:38:36 +0100

    Remove None CoopSpaceRole

--- src/app/shared/model/coop-spaces.ts
+++ src/app/shared/model/coop-spaces.ts
@@ -10,7 +10,6 @@ export interface CoopSpace {
 }
 
 export enum CoopSpaceRole {
-  None = 'NONE',  // MinIO admins can see CoopSpaces they are not a member of.
   Guest = 'GUEST',
   User = 'USER',
   Admin = 'ADMIN',
