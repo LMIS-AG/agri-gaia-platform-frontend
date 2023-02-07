0677471de19d5ee43422b1e1ab202e17791b09f2
Julian-Marco <93761466+Julian-Marco@users.noreply.github.com>
2023-02-07 14:20:11 +0100

    Feature/56951 teilnehmer loeschen (#22)
    * First try, not working yet
    
    * Modification to delete the user from the Keycloak group at least
    
    * Further optimization
    
    * ...
    
    * Retrieve and send MemberId, delete user from table
    
    * add confirmation question
    
    * added confirmation / error message when deleting a member
    
    * user now gets redirected if coop space url is not accessible (not authorized or not existing)
    
    * Only admins are allowed to delete members from now on
    
    * cleanup
    
    * Admins shouldn't be allowed to delete themselves
    
    * change function name

--- src/app/data-management/pages/coop-spaces/coop-space-details/coop-space-details.component.ts
+++ src/app/data-management/pages/coop-spaces/coop-space-details/coop-space-details.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { concatMap, filter, map, switchMap } from 'rxjs';
-import { CoopSpace } from 'src/app/shared/model/coop-spaces';
+import { catchError, concatMap, filter, map, of, switchMap, throwError } from 'rxjs';
+import { CoopSpace, CoopSpaceRole } from 'src/app/shared/model/coop-spaces';
 import { GeneralPurposeAsset } from 'src/app/shared/model/coopSpaceAsset';
 import { CoopSpacesService } from '../coop-spaces.service';
+import { Member } from 'src/app/shared/model/member';
+import { UIService } from 'src/app/shared/services/ui.service';
+import { translate } from '@ngneat/transloco';
+import { AuthenticationService } from 'src/app/core/authentication/authentication.service';
 
 @Component({
   selector: 'app-coop-space-details',
@@ -17,32 +21,78 @@ export class CoopSpaceDetailsComponent implements OnInit {
   public displayedColumnsDataset: string[] = ['name', 'date', 'more'];
   public datasetDatasource: GeneralPurposeAsset[] = [];
 
-  constructor(private route: ActivatedRoute, private router: Router, private coopSpacesService: CoopSpacesService) {}
+  public userName: string | undefined;
+  public fullName: string | undefined;
 
-  public ngOnInit(): void {
-    this.route.paramMap
-      .pipe(
-        filter(paramMap => paramMap.has('id')),
-        map(paramMap => parseInt(paramMap.get('id')!, 10)),
-        switchMap(id =>
-          this.coopSpacesService
-            .getCoopSpaceById(id)
-            .pipe(
-              concatMap(coopSpace =>
-                this.coopSpacesService.getAssets(coopSpace.id!).pipe(map(assets => ({ coopSpace, assets })))
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private coopSpacesService: CoopSpacesService,
+    private uiService: UIService,
+    private authenticationService: AuthenticationService
+    ) {}
+
+    public ngOnInit(): void {
+      this.route.paramMap
+        .pipe(
+          filter(paramMap => paramMap.has('id')),
+          map(paramMap => parseInt(paramMap.get('id')!, 10)),
+          switchMap(id =>
+            this.coopSpacesService
+              .getCoopSpaceById(id)
+              .pipe(
+                concatMap(coopSpace =>
+                  this.coopSpacesService.getAssets(coopSpace.id!).pipe(map(assets => ({ coopSpace, assets })))
+                )
               )
-            )
+          )
         )
-      )
-      .subscribe(result => {
-        if (result.coopSpace != null) {
-          this.coopSpace = result.coopSpace;
-        } else {
-          this.router.navigateByUrl('/coopspaces');
+        .subscribe({
+          next: result => {
+            if (result.coopSpace != null) {
+              this.coopSpace = result.coopSpace;
+            } 
+            this.datasetDatasource = result.assets;
+          },
+          error: error => {
+            console.error(error);
+            this.router.navigateByUrl('/coopspaces');
+          }
+        });
+        // retrieve the username of the user that is currently logged in
+        this.authenticationService.userProfile$.subscribe(userProfile => {
+          if (userProfile === null) throw Error("userProfile was null.")
+          this.userName = userProfile.username;
+          this.fullName = `${userProfile.firstName} ${userProfile.lastName}`
+        });
+    }
+    
+  public onDeleteMember(member: Member): void {
+    this.uiService
+    .confirm(`${member.name}`, translate('dataManagement.coopSpaces.details.dialog.deleteMemberConfirmationQuestion'), {
+      buttonLabels: 'confirm',
+      confirmButtonColor: 'primary',
+    })
+    .subscribe(result => {
+      if (result) {
+        // modify the string before sending it, e.g. "USER" becomes "User"
+        let role = member.role.toLowerCase();
+        role = role.charAt(0).toUpperCase() + role.slice(1);
+  
+      // send the necessary data and remove the user from the member table 
+      this.coopSpacesService.deleteMember(member.id!, member.username, role, this.coopSpace!.name, this.coopSpace!.company)
+      .subscribe({
+        next: () => {
+          this.coopSpace!.members = this.coopSpace!.members.filter(m => m.id !== member.id);
+          this.uiService.showSuccessMessage(translate('dataManagement.coopSpaces.details.dialog.deleteMemberConfirmationText'));
+        },
+        error: () => {
+          this.uiService.showErrorMessage(translate('dataManagement.coopSpaces.details.dialog.deleteMemberErrorText'));
         }
-        this.datasetDatasource = result.assets;
       });
-  }
+    }
+  });
+}
 
   public openSettings(): void {
     throw Error('Not yet implemented');
@@ -68,25 +118,33 @@ export class CoopSpaceDetailsComponent implements OnInit {
     throw Error('Not yet implemented');
   }
 
-  // TODO
-  public getUsedImage(index: number): string {
-    let filename = '';
-    if (index === 1) {
-      filename = 'Bitmap_jupyter_notebook.png';
-    }
-    if (index === 2) {
-      filename = 'Bitmap_harbor_ai.png';
-    }
-    if (index === 3) {
-      filename = 'Bitmap_argo_cd.png';
-    }
-    return `assets/tools_png/${filename}`;
-  }
-
   public openInFull(): void {
     this.router.navigate(['focus'], {
       relativeTo: this.route,
       state: { datasetDatasource: this.datasetDatasource },
     });
   }
+
+  public handleDeleteMemberSuccess(): void {
+    this.uiService.showSuccessMessage(translate('dataManagement.coopSpaces.details.dialog.deleteMemberConfirmationText'))
+  }
+
+  public handleDeleteMemberError(): void {
+    this.uiService.showSuccessMessage(translate('dataManagement.coopSpaces.details.dialog.deleteMemberErrorText'))
+  }  
+
+  public getUserRole(): CoopSpaceRole {
+    let member = this.coopSpace!.members.find(m => m.username === this.userName)
+    if (member === undefined) return CoopSpaceRole.None;
+    return member.role;
+  }
+
+  public isAdmin(): boolean {
+    return this.getUserRole() === CoopSpaceRole.Admin
+  }   
+
+  public returnFullName(): String | undefined  {
+    return this.fullName
+  }
+
 }
--- src/app/data-management/pages/coop-spaces/coop-spaces.service.ts
+++ src/app/data-management/pages/coop-spaces/coop-spaces.service.ts
@@ -39,4 +39,8 @@ export class CoopSpacesService {
   public getAssets(id: number): Observable<GeneralPurposeAsset[]> {
     return this.http.get<GeneralPurposeAsset[]>(`${environment.backend.url}/coopspaces/${id}/assets`);
   }
+
+  public deleteMember(memberId: Number, username: String, role: String, coopSpaceName: String, companyName: String): Observable<void> {
+    return this.http.post<void>(`${environment.backend.url}/coopspaces/deleteMember`, {memberId, username, role, coopSpaceName, companyName});
+  }
 }
