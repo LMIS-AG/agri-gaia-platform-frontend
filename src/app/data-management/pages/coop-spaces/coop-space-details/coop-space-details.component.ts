6d3c32dfff072f0ca51ec2f92fbec27599cd6b27
mgr <mgr@lmis.de>
2023-02-14 10:09:26 +0100

    removed fullname method

--- src/app/data-management/pages/coop-spaces/coop-space-details/coop-space-details.component.ts
+++ src/app/data-management/pages/coop-spaces/coop-space-details/coop-space-details.component.ts
@@ -23,99 +23,111 @@ export class CoopSpaceDetailsComponent implements OnInit {
 
   public userName: string | undefined;
   public fullName: string | undefined;
-  public originalRole: string = "";
+  public originalRole: string = '';
   public roles: CoopSpaceRole[] = [CoopSpaceRole.Admin, CoopSpaceRole.User, CoopSpaceRole.Guest];
 
   constructor(
-    private route: ActivatedRoute, 
-    private router: Router, 
+    private route: ActivatedRoute,
+    private router: Router,
     private coopSpacesService: CoopSpacesService,
     private uiService: UIService,
     private authenticationService: AuthenticationService
-    ) {}
+  ) {}
 
-    public ngOnInit(): void {
-      this.route.paramMap
-        .pipe(
-          filter(paramMap => paramMap.has('id')),
-          map(paramMap => parseInt(paramMap.get('id')!, 10)),
-          switchMap(id =>
-            this.coopSpacesService
-              .getCoopSpaceById(id)
-              .pipe(
-                concatMap(coopSpace =>
-                  this.coopSpacesService.getAssets(coopSpace.id!).pipe(map(assets => ({ coopSpace, assets })))
-                )
+  public ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        filter(paramMap => paramMap.has('id')),
+        map(paramMap => parseInt(paramMap.get('id')!, 10)),
+        switchMap(id =>
+          this.coopSpacesService
+            .getCoopSpaceById(id)
+            .pipe(
+              concatMap(coopSpace =>
+                this.coopSpacesService.getAssets(coopSpace.id!).pipe(map(assets => ({ coopSpace, assets })))
               )
-          )
+            )
         )
-        .subscribe({
-          next: result => {
-            if (result.coopSpace != null) {
-              this.coopSpace = result.coopSpace;
-            } 
-            this.datasetDatasource = result.assets;
-          },
-          error: error => {
-            console.error(error);
-            this.router.navigateByUrl('/coopspaces');
+      )
+      .subscribe({
+        next: result => {
+          if (result.coopSpace != null) {
+            this.coopSpace = result.coopSpace;
           }
-        });
-        // retrieve the username of the user that is currently logged in
-        this.authenticationService.userProfile$.subscribe(userProfile => {
-          if (userProfile === null) throw Error("userProfile was null.")
-          this.userName = userProfile.username;
-          this.fullName = `${userProfile.firstName} ${userProfile.lastName}`
-        });
-    }
-    
-    public onDeleteMember(member: Member): void {
-      this.uiService
-      .confirm(`${member.name}`, translate('dataManagement.coopSpaces.details.dialog.deleteMemberConfirmationQuestion'), {
-        buttonLabels: 'confirm',
-        confirmButtonColor: 'primary',
-      })
-      .subscribe(result => {
-        if (result) {
-    
-        // send the necessary data and remove the user from the member table 
-        this.coopSpacesService.deleteMember(this.coopSpace!.name, member)
-        .subscribe({
-          next: () => {
-            this.coopSpace!.members = this.coopSpace!.members.filter(m => m.id !== member.id);
-            this.uiService.showSuccessMessage(translate('dataManagement.coopSpaces.details.dialog.deleteMemberConfirmationText'));
-          },
-          error: () => {
-            this.uiService.showErrorMessage(translate('dataManagement.coopSpaces.details.dialog.deleteMemberErrorText'));
-          }
-        });
-      }
+          this.datasetDatasource = result.assets;
+        },
+        error: error => {
+          console.error(error);
+          this.router.navigateByUrl('/coopspaces');
+        },
+      });
+    // retrieve the username of the user that is currently logged in
+    this.authenticationService.userProfile$.subscribe(userProfile => {
+      if (userProfile === null) throw Error('userProfile was null.');
+      this.userName = userProfile.username;
+      this.fullName = `${userProfile.firstName} ${userProfile.lastName}`;
     });
   }
 
-// change the role of a user and thereby its respective rights
-public onRoleChange(originalRole: string, member: Member) {
+  public onDeleteMember(member: Member): void {
     this.uiService
-    .confirm(`${member.name}`, translate('dataManagement.coopSpaces.details.dialog.changeMemberRoleConfirmationQuestion'), {
-      buttonLabels: 'confirm',
-      confirmButtonColor: 'primary',
-    })
-    .subscribe(result => {
-      if (result) {
-        // send the necessary data, originalRole must be included for finding the appropiate Keycloak group and deleting the user from it
-        this.coopSpacesService.changeMemberRole(this.coopSpace!.id!, originalRole, member)
-        .subscribe({
-          next: () => {
-            this.uiService.showSuccessMessage(translate('dataManagement.coopSpaces.details.dialog.changeMemberRoleConfirmationText'));
-          },
-          error: () => {
-            this.uiService.showErrorMessage(translate('dataManagement.coopSpaces.details.dialog.changeMemberRoleErrorText'));
-          }
-        });
-      }
-    })
+      .confirm(
+        `${member.name}`,
+        translate('dataManagement.coopSpaces.details.dialog.deleteMemberConfirmationQuestion'),
+        {
+          buttonLabels: 'confirm',
+          confirmButtonColor: 'primary',
+        }
+      )
+      .subscribe(result => {
+        if (result) {
+          // send the necessary data and remove the user from the member table
+          this.coopSpacesService.deleteMember(this.coopSpace!.name, member).subscribe({
+            next: () => {
+              this.coopSpace!.members = this.coopSpace!.members.filter(m => m.id !== member.id);
+              this.uiService.showSuccessMessage(
+                translate('dataManagement.coopSpaces.details.dialog.deleteMemberConfirmationText')
+              );
+            },
+            error: () => {
+              this.uiService.showErrorMessage(
+                translate('dataManagement.coopSpaces.details.dialog.deleteMemberErrorText')
+              );
+            },
+          });
+        }
+      });
   }
 
+  // change the role of a user and thereby its respective rights
+  public onRoleChange(originalRole: string, member: Member) {
+    this.uiService
+      .confirm(
+        `${member.name}`,
+        translate('dataManagement.coopSpaces.details.dialog.changeMemberRoleConfirmationQuestion'),
+        {
+          buttonLabels: 'confirm',
+          confirmButtonColor: 'primary',
+        }
+      )
+      .subscribe(result => {
+        if (result) {
+          // send the necessary data, originalRole must be included for finding the appropiate Keycloak group and deleting the user from it
+          this.coopSpacesService.changeMemberRole(this.coopSpace!.id!, originalRole, member).subscribe({
+            next: () => {
+              this.uiService.showSuccessMessage(
+                translate('dataManagement.coopSpaces.details.dialog.changeMemberRoleConfirmationText')
+              );
+            },
+            error: () => {
+              this.uiService.showErrorMessage(
+                translate('dataManagement.coopSpaces.details.dialog.changeMemberRoleErrorText')
+              );
+            },
+          });
+        }
+      });
+  }
 
   public openSettings(): void {
     throw Error('Not yet implemented');
@@ -149,25 +161,22 @@ public onRoleChange(originalRole: string, member: Member) {
   }
 
   public handleDeleteMemberSuccess(): void {
-    this.uiService.showSuccessMessage(translate('dataManagement.coopSpaces.details.dialog.deleteMemberConfirmationText'))
+    this.uiService.showSuccessMessage(
+      translate('dataManagement.coopSpaces.details.dialog.deleteMemberConfirmationText')
+    );
   }
 
   public handleDeleteMemberError(): void {
-    this.uiService.showSuccessMessage(translate('dataManagement.coopSpaces.details.dialog.deleteMemberErrorText'))
-  }  
+    this.uiService.showSuccessMessage(translate('dataManagement.coopSpaces.details.dialog.deleteMemberErrorText'));
+  }
 
   public getUserRole(): CoopSpaceRole | undefined {
-    let member = this.coopSpace!.members.find(m => m.username === this.userName)
+    let member = this.coopSpace!.members.find(m => m.username === this.userName);
     if (member === undefined) return undefined;
     return member.role;
   }
 
   public isAdmin(): boolean {
-    return this.getUserRole() === CoopSpaceRole.Admin
-  }   
-
-  public returnFullName(): String | undefined  {
-    return this.fullName
+    return this.getUserRole() === CoopSpaceRole.Admin;
   }
-
 }
