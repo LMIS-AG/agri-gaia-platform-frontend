74840051e52be02a898c7f259f250860acbf45c7
Marcel Ruland <26412792+marcelruland@users.noreply.github.com>
2023-02-28 11:19:04 +0100

    Bugfix/fix filesize conversion (#33)
    * Rewrite prettyPrintFileSize
    
    * Add tests for prettyPrintFileSize

--- src/app/shared/utils/convert-utils.ts
+++ src/app/shared/utils/convert-utils.ts
@@ -1,18 +1,8 @@
-export function prettyPrintFileSize(size: string):string {
-    if (!isNaN(Number(size))) {
-      if (size.length < 4) {
-        return size + ' B';
-      }
-      else if (4 <= size.length && size.length < 8) {
-        return (Number(size) / 1024 ).toFixed(1) + ' KiB';
-      }
-      else if (8 <= size.length && size.length < 12) {
-        return (Number(size) / (1024**2)).toFixed(1) + ' MiB';
-      }
-      else if (12 <= size.length && size.length < 16) {
-        return (Number(size) / (1024**3)).toFixed(1) + ' GiB';
-      }
-    }
-
-    return "";
+export function prettyPrintFileSize(size: number):string {
+  if (isNaN(size)) return "";
+  if (size < 2 ** 10) return `${size} B`
+  if (size < 2 ** 20) return (size / 2 ** 10).toFixed(1) + " KiB"
+  if (size < 2 ** 30) return (size / 2 ** 20).toFixed(1) + " MiB"
+  if (size < 2 ** 40) return (size / 2 ** 30).toFixed(1) + " GiB"
+  return (size / 2 ** 40).toFixed(1) + " TiB"
 }
