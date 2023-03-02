153dbc19adef3a5a9bf4b0e9fd309acd79706eb5
mgr <mgr@lmis.de>
2023-03-02 13:51:36 +0100

    implemented 1st iteration of page 1

--- src/app/data-management/pages/buckets/assets/publish-asset-dlg/publish-asset-dlg.component.ts
+++ src/app/data-management/pages/buckets/assets/publish-asset-dlg/publish-asset-dlg.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AssetType } from 'src/app/shared/model/asset-type';
 import { GeneralPurposeAsset } from 'src/app/shared/model/coopSpaceAsset';
+import { $enum } from 'ts-enum-util';
 
 @Component({
   selector: 'app-publish-asset-dlg',
@@ -8,7 +11,21 @@ import { GeneralPurposeAsset } from 'src/app/shared/model/coopSpaceAsset';
   styleUrls: ['./publish-asset-dlg.component.scss'],
 })
 export class PublishAssetDlgComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) data: GeneralPurposeAsset, protected dialogRef: MatDialogRef<any>) {}
+  public formGroup!: FormGroup;
+  public assetTypes: AssetType[] = $enum(AssetType).getValues();
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) data: GeneralPurposeAsset,
+    protected dialogRef: MatDialogRef<any>,
+    private formBuilder: FormBuilder
+  ) {
+    this.formGroup = this.formBuilder.group({
+      id: ['', Validators.required],
+      name: ['', [Validators.required, Validators.maxLength(63)]],
+      description: ['', [Validators.maxLength(300)]],
+      assetType: [null],
+    });
+  }
 
   public ngOnInit(): void {}
 
