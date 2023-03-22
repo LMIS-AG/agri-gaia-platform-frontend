b16c43c4eae0df2bfe02742443c0f94ae9ad341f
mgr <mgr@lmis.de>
2023-03-22 11:25:32 +0100

    adjusted creation of coopSpaces. New entries are now directly displayed ater creation.

--- src/app/data-management/pages/coop-spaces/coop-spaces.service.ts
+++ src/app/data-management/pages/coop-spaces/coop-spaces.service.ts
@@ -20,8 +20,8 @@ export class CoopSpacesService {
     return this.http.get<CoopSpace>(`${environment.backend.url}/coopspaces/${id}`);
   }
 
-  public create(coopSpace: CoopSpace): Observable<void> {
-    return this.http.post<void>(environment.backend.url + '/coopspaces', coopSpace);
+  public create(coopSpace: CoopSpace): Observable<CoopSpace> {
+    return this.http.post<CoopSpace>(environment.backend.url + '/coopspaces', coopSpace);
   }
 
   public getSelectableMembers(): Observable<Member[]> {
