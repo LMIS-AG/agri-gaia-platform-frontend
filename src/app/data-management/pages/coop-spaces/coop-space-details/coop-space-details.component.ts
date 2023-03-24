0ab8a8646a67980af5e6519855007a36a330385a
mgr <mgr@lmis.de>
2023-03-24 14:24:50 +0100

    added loading spinner for removing and addition of members

--- src/app/data-management/pages/coop-spaces/coop-space-details/coop-space-details.component.ts
+++ src/app/data-management/pages/coop-spaces/coop-space-details/coop-space-details.component.ts
@@ -37,8 +37,10 @@ export class CoopSpaceDetailsComponent implements OnInit {
   public roles: CoopSpaceRole[] = $enum(CoopSpaceRole).getValues();
 
   public bucket?: string;
-  public isLoading = false;
+  public isUploading = false;
   public isDeletingAsset: boolean = false;
+  public isDeletingMember: boolean = false;
+  public isAddingMember: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -99,17 +101,20 @@ export class CoopSpaceDetailsComponent implements OnInit {
       .subscribe(result => {
         if (result) {
           // send the necessary data and remove the user from the member table
+          this.isDeletingMember = true;
           this.coopSpacesService.deleteMember(this.coopSpace!.name, member).subscribe({
             next: () => {
               this.memberDatasource.data = this.memberDatasource.data.filter(m => m.id !== member.id);
               this.uiService.showSuccessMessage(
                 translate('dataManagement.coopSpaces.details.dialog.deleteMemberConfirmationText')
               );
+              this.isDeletingMember = false;
             },
             error: () => {
               this.uiService.showErrorMessage(
                 translate('dataManagement.coopSpaces.details.dialog.deleteMemberErrorText')
               );
+              this.isDeletingMember = false;
             },
           });
         }
@@ -160,7 +165,7 @@ export class CoopSpaceDetailsComponent implements OnInit {
     const bucket = this.bucket;
     if (bucket == null) throw Error('Bucket was null in addFile().');
 
-    this.isLoading = true;
+    this.isUploading = true;
     this.bucketService.buildFormDataAndUploadAssets(event, bucket).subscribe({
       complete: () => this.handleUploadSuccess(),
       error: () =>
@@ -195,15 +200,18 @@ export class CoopSpaceDetailsComponent implements OnInit {
   }
 
   public addMember(membersSelected: Member[]): void {
+    this.isAddingMember = true;
     this.coopSpacesService.addMember(this.coopSpace?.id!, membersSelected).subscribe({
       complete: () => {
         this.reloadMembersListAndUpdateMembersDataSource();
         this.uiService.showSuccessMessage(
           translate('dataManagement.coopSpaces.details.dialog.addMemberConfirmationText')
         );
+        this.isAddingMember = false;
       },
       error: () => {
         this.uiService.showErrorMessage(translate('dataManagement.coopSpaces.details.dialog.addMemberErrorText'));
+        this.isAddingMember = false;
       },
     });
   }
@@ -263,7 +271,7 @@ export class CoopSpaceDetailsComponent implements OnInit {
   }
 
   private handleUploadSuccess(): void {
-    this.isLoading = false;
+    this.isUploading = false;
 
     this.uiService.showSuccessMessage(translate('dataManagement.coopSpaces.details.dialog.uploadedFile'));
     if (this.bucket) {
