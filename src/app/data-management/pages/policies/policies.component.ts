07299aa7347b0e5e418d5ac3d356cb9f7f7f9d21
Marcel Ruland <info@marcelruland.com>
2023-05-16 08:18:17 +0200

    Delete policies

--- src/app/data-management/pages/policies/policies.component.ts
+++ src/app/data-management/pages/policies/policies.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CoopSpace } from 'src/app/shared/model/coop-spaces';
-import { Policy } from 'src/app/shared/model/policy';
-import { PolicyService } from './policy.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CoopSpace} from 'src/app/shared/model/coop-spaces';
+import {Policy} from 'src/app/shared/model/policy';
+import {PolicyService} from './policy.service';
 
 @Component({
   selector: 'app-policies',
@@ -16,19 +16,24 @@ export class PoliciesComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, private policyService: PolicyService) {}
 
   public ngOnInit(): void {
+    this.initPolicyList();
+  }
+
+  private initPolicyList(): void {
     this.policyService.getAllPolicies().subscribe(policies => {
       this.dataSource = policies;
     });
   }
 
   public addPolicy(): void {
-    this.router.navigate(['create'], { relativeTo: this.route });
+    this.router.navigate(['create'], {relativeTo: this.route});
   }
 
-  public onDelete(): void {
-    throw Error('Not yet implemented');
+  public onDelete(policyName: string): void {
+    this.policyService.deletePolicy(policyName).subscribe(() => this.initPolicyList())
   }
 
   // TODO potentially useful in next Story
-  public openDetails(row: CoopSpace): void {}
+  public openDetails(row: CoopSpace): void {
+  }
 }
--- src/app/data-management/pages/policies/policy.service.ts
+++ src/app/data-management/pages/policies/policy.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { Policy, PolicyType } from 'src/app/shared/model/policy';
-import {HttpClient} from "@angular/common/http";
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Policy} from 'src/app/shared/model/policy';
+import {HttpClient, HttpResponse} from "@angular/common/http";
 import {environment} from "../../../../environments/environment";
 
 @Injectable({
@@ -13,5 +13,10 @@ export class PolicyService {
 
   public getAllPolicies(): Observable<Policy[]> {
     return this.http.get<Policy[]>(`${environment.backend.url}/edc/policies`)
+  }
+
+  public deletePolicy(policyName: string): Observable<HttpResponse<unknown>> {
+    return this.http.delete(`${environment.backend.url}/edc/policies/${policyName}`,
+      {observe: 'response'});
   }
 }
